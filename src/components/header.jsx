c1a1c83712df65fe23b85476612d729da79e02e7
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-26 18:37:38 -0300

    Req-09: Adiciona pages Foods e Login. Cria componente Header.

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+
+export default function Header({ title }) {
+  return (
+    <div>
+      <Button type="button" data-testid="profile-top-btn">
+        <img src="../images/profileIcon.svg" alt="Profile Icon" />
+      </Button>
+      <h1 data-testid="page-title">{title}</h1>
+      <Button
+        type="button"
+        data-testid="search-top-btn"
+      >
+        <img
+          src="../images/exploreIcon.svg"
+          alt="Explore Icon"
+        />
+      </Button>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}.isRequired;
