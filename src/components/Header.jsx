2715452d879df4c47e8d7cc2ffdde1e00ceee2d1
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-26 19:09:52 -0300

    Req-10: Adicionando páginas ao app.-

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 // import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Link from 'react-router-dom';
 
 export default function Header({ title }) {
   return (
     <div>
-      <Button type="button" data-testid="profile-top-btn">
-        <img src="../images/profileIcon.svg" alt="Profile Icon" />
-      </Button>
+      <Link to="/profile">
+        <Button
+          type="button"
+          data-testid="profile-top-btn"
+        >
+          <img src="../images/profileIcon.svg" alt="Profile Icon" />
+        </Button>
+      </Link>
       <h1 data-testid="page-title">{title}</h1>
       <Button
         type="button"
