b60c50d011886dd2da9a27b68b0e94e71c8360d1
Juliana Giusti <juliana.vmelo@gmail.com>
2022-04-27 17:45:29 -0300

    Req 13 radios e button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,6 +7,48 @@ export default function SearchBar() {
         data-testid="search-input"
         type="text"
       />
+      <div className="d-flex justify-content-evenly">
+        <label
+          htmlFor="ingredient"
+        >
+          <input
+            type="radio"
+            data-testid="ingredient-search-radio"
+            id="ingredient"
+            value="ingredient"
+          />
+          Ingredient
+        </label>
+        <label
+          htmlFor="name"
+        >
+          <input
+            type="radio"
+            data-testid="name-search-radio"
+            id="name"
+            value="name"
+          />
+          Name
+        </label>
+        <label
+          htmlFor="first-letter"
+        >
+          <input
+            type="radio"
+            data-testid="first-letter-search-radio"
+            id="first-letter"
+            value="first-letter"
+          />
+          First Letter
+        </label>
+      </div>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ () => {} }
+      >
+        Buscar
+      </button>
     </div>
   );
 }
