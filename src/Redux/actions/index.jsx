8d13b796da3b5b66f39e2e69d8c7ed3f0360e2ac
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-27 18:41:59 -0300

    Req-26: Cria fetch para receitas genericas. Cria action assíncrona, ação para salvar receitas e ação de loading."

--- src/Redux/actions/index.jsx
+++ src/Redux/actions/index.jsx
@@ -1,5 +1,30 @@
-import { USER_LOGIN, USER_LOGOUT01, ACTIVE_SEARCH_BAR } from './actionTypes';
+import fetchGenericRecepies from '../../services/fetchGenericRecepies';
+import {
+  USER_LOGIN, USER_LOGOUT01,
+  ACTIVE_SEARCH_BAR,
+  SAVE_RECEPIES,
+  LOADING } from './actionTypes';
 
 export const minhaAction = (value) => ({ type: USER_LOGIN, value });
 export const minhaAction01 = (value) => ({ type: USER_LOGOUT01, value });
 export const searchBar = () => ({ type: ACTIVE_SEARCH_BAR });
+
+export const loading = () => ({
+  type: LOADING,
+});
+
+export const saveRecepies = (payload) => ({
+  type: SAVE_RECEPIES, payload,
+});
+
+export function actFetchGenericRecepies() {
+  return async (dispatch) => {
+    dispatch(loading());
+    const response = await fetchGenericRecepies();
+    if (response.status === 'ok') {
+      const numberOfRecepies = 12;
+      const recipes = response.data.meals.slice(0, numberOfRecepies);
+      dispatch(saveRecepies(recipes));
+    }
+  };
+}
---
+++ src/services/fetchGenericRecepies.js
@@ -0,0 +1,15 @@
+const fetchGenericRecepies = async (select) => {
+  let url = '';
+  if (select === 'foods') { url = 'https://www.themealdb.com/api/json/v1/1/search.php?s='; }
+  if (select === 'drinks') { url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='; }
+  try {
+    const resolve = await fetch(url);
+    const data = await resolve.json();
+    return { status: 'ok', data };
+  } catch (error) {
+    console.log(error);
+    return { status: 'error', data: error };
+  }
+};
+
+export default fetchGenericRecepies;
