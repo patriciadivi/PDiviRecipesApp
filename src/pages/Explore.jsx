114d1a081c0fc2a9924fe23f29b4a84f4be41511
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-30 08:46:22 -0300

    Req-69: ao clicar nos botoes redireciona para as pages de explorar comida ou drinks.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Explore() {
+  const history = useHistory();
   return (
     <section>
       <Header title="Explorer" searchEnabled={ false } />
       <div className="d-flex flex-column">
         <Button
-          variant="light mt-3 mx-3"
+          variant="light"
           data-testid="explore-foods"
           size="lg"
+          className="mt-3 mx-3"
+          onClick={ () => history.push('/explore/foods') }
         >
           Explore Foods
-
         </Button>
         <Button
-          variant="light mt-3 mx-3"
+          variant="light"
           data-testid="explore-drinks"
           size="lg"
+          className="mt-3 mx-3"
+          onClick={ () => history.push('/explore/drinks') }
         >
           Explore Drinks
 
