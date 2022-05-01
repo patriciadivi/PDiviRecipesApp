39d260eebff6bf9c9b5e7b03d262f1aaad491020
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-01 07:44:44 -0300

    Req-74: cria fetch para pegar receita aleatoria.

--- src/components/BtnsExploreRecepies.jsx
+++ src/components/BtnsExploreRecepies.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import fetchSurprise from '../services/fetchSurprise';
 
 export default function BtnsExploreRecepies(props) {
   const {
     type,
   } = props;
   const history = useHistory();
+
+  const gerRandomRecepie = async () => {
+    const { status, data } = await fetchSurprise(type);
+    if (status === 'ok' && type === 'foods') {
+      history.push(`/${type}/${data.meals.idMeal}`);
+    }
+    if (status === 'ok' && type === 'drinks') {
+      history.push(`/${type}/${data.drinks.idDrink}`);
+    }
+  };
+
   return (
     <div>
       <Button
@@ -35,7 +47,7 @@ export default function BtnsExploreRecepies(props) {
         data-testid="explore-surprise"
         size="lg"
         className="mt-3 mx-3"
-        // onClick={ () => history.push('/explore/foods') }
+        onClick={ () => gerRandomRecepie() }
       >
         Surprise me!
       </Button>
