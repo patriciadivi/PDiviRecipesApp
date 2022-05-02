fe20dd1ebc9e73aa087ccd343b7feb97566f8e5c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-02 13:43:58 -0300

    Req-78: Adiciona dropdow a page exploreNationalities.

--- src/pages/ExploreNacionalities.jsx
+++ src/pages/ExploreNacionalities.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/components/Header.css';
@@ -7,6 +8,17 @@ export default function ExploreNacionalities() {
   return (
     <div>
       <Header title="Explore Nationalities" searchEnabled />
+      <DropdownButton
+        id="dropdown-basic-button"
+        className="mt-3 mx-auto "
+        title="Select Nationality"
+        size="lg"
+        variant="secondary"
+      >
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </DropdownButton>
       <div>
         <Footer />
       </div>
