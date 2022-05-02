d9b4740bf66853365ad85bb5d7d2da71120157c5
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-02 18:01:30 -0300

    Req-78: Deixa de usar DropdownButton e passa a utilizar select convencional devido aos testes.

--- src/components/DropdownNationalities.jsx
+++ src/components/DropdownNationalities.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes, { string } from 'prop-types';
-import { DropdownButton, Dropdown } from 'react-bootstrap';
+// import { DropdownButton, Dropdown } from 'react-bootstrap';
 import randomIdNumber from '../services/randomIdNumber';
 import { actFetchByNationality } from '../Redux/actions';
 
@@ -9,25 +9,27 @@ export default function DropdownNationalities({ natList, type }) {
   const dispatch = useDispatch();
   return (
     <div className="d-flex jus justify-content-center">
-      <DropdownButton
-        className="mt-3 mx-3 "
-        title="Select Nationality"
-        size="lg"
-        variant="light"
+      <select
+        className="mt-3 mx-3 light"
+        // title="Select Nationality"
+        // size="lg"
+        // variant="light"
         data-testid="explore-by-nationality-dropdown"
-        renderMenuOnMount
-        // menuRole="menu"
+        // renderMenuOnMount
+        defaultValue="default"
+
       >
+        <option value="default" disabled>Select Nationality</option>
         {natList.map((nat) => (
-          <Dropdown.Item
+          <option
             key={ nat + randomIdNumber() }
             data-testid={ `${nat}-option` }
             value={ nat }
             onClick={ () => dispatch(actFetchByNationality(type, nat)) }
           >
             {nat}
-          </Dropdown.Item>))}
-      </DropdownButton>
+          </option>))}
+      </select>
     </div>
   );
 }
