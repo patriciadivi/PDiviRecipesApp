15ec3363464a61a95f14dcad4adc3efb215c9be8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-03 17:41:23 -0300

    Req-82-83-85-86-87: Adiciona componentes a page Profile. Com o botão logout limpa storage e muda para /. Os botões FavoriteRecepies e Done Recepies redirecionam para as respectivas pages. O Req-84 depende da implementação das pages Favorite Recepies e Done Recepies.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/components/Header.css';
 
 export default function Profile() {
+  const history = useHistory();
+  const [email, setMail] = useState('');
+
+  const cleanStorageAndRedirect = () => {
+    window.localStorage.clear();
+    history.push('/');
+  };
+
+  useEffect(() => {
+    const user = JSON.parse(window.localStorage.getItem('user'));
+    setMail(() => user.email);
+  }, []);
+
   return (
     <div>
       <Header title="Profile" />
+      <div className="d-flex flex-column">
+        <h2 className="mx-auto my-5" data-testid="profile-email">{email}</h2>
+        <Button
+          variant="light"
+          data-testid="profile-done-btn"
+          size="lg"
+          className="mt-3 mx-auto"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recepies
+        </Button>
+        <Button
+          variant="light"
+          data-testid="profile-favorite-btn"
+          size="lg"
+          className="mt-3 mx-auto"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recepies
+        </Button>
+        <Button
+          variant="light"
+          data-testid="profile-logout-btn"
+          size="lg"
+          className="mt-3 mx-auto"
+          onClick={ () => cleanStorageAndRedirect() }
+        >
+          Logout
+        </Button>
+      </div>
+
       <Footer />
     </div>
   );
