f558be87e9b782e9cd04a4f3da199cd81b87fd10
PatriciaDivi <patriciavianad@gmail.com>
2022-05-04 09:16:26 -0300

    botões de categorias iniciados a estilização

---
+++ src/components/DescriptionOfRecipes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import fetchSurprise from '../services/fetchSurprise';
+
+export default function DescriptionOfRecipes() {
+  return (
+    <section>
+      <header>
+        <img
+          src=""
+          alt=""
+          data-testid="recipe-photo"
+        />
+      </header>
+      <h1>DescriptionOfRecipes</h1>
+    </section>
+  );
+}
--- src/pages/Recepie.jsx
+++ src/pages/Recepie.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import DescriptionOfRecipes from '../components/DescriptionOfRecipes';
 import '../styles/components/Header.css';
 
 export default function Recepie() {
   return (
     <section>
       <Header title="Recepie" />
+      <DescriptionOfRecipes />
     </section>
   );
 }
