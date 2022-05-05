f182fd5f5bdf50143127711d3d4ca6c563c5ad36
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-05 15:14:26 -0300

    Req-40: cria funcao auxiliar para checar se receita já foi iniciada.

---
+++ src/services/checkRecepieStarted.jsx
@@ -0,0 +1,14 @@
+const checkRecepieStart = (id) => {
+  let doneRecepie = false;
+  const tempData = window.localStorage.getItem('doneRecipes');
+  let doneRecepies = [];
+  if (tempData) { doneRecepies = JSON.parse(tempData); }
+  if (doneRecepies) {
+    doneRecepies.forEach((element) => {
+      if (element.id === id) { doneRecepie = true; }
+    });
+  }
+  return doneRecepie;
+};
+
+export default checkRecepieStart;
