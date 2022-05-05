77d7251ce4e7419671f4cf02e4450912605be46c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-05 17:10:52 -0300

    Req-41: Cria pagina RecepieInProgress

---
+++ src/pages/RecepieInProgres.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+import '../styles/components/Header.css';
+
+export default function RecepieInProgres() {
+  return (
+    <div>
+      <Header title="RecepieInProgres" searchEnabled={ false } />
+    </div>
+  );
+}
