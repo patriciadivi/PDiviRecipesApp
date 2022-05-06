77e54676e3b4c6a3364476170f43499192462749
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-06 19:12:08 -0300

    Solve problem of non existent id or key.

--- src/services/saveProgress.js
+++ src/services/saveProgress.js
@@ -2,10 +2,10 @@ const saveProgress = (type, id, ingredient, checked) => {
   const key = type === 'foods' ? 'meals' : 'cocktails';
   const savedData = window.localStorage.getItem('inProgressRecipes');
   if (savedData) {
-    // if (tempData[key][id]) {
     const newObj = JSON.parse(savedData);
+    if (!newObj[key]) { newObj[key] = {}; }
+    if (!newObj[key][id]) { newObj[key][id] = []; }
     if (checked) {
-      console.log(newObj[key][id]);
       newObj[key][id] = [...newObj[key][id], ingredient];
     } else {
       const newArray = newObj[key][id].filter((e) => e !== ingredient);
@@ -13,7 +13,6 @@ const saveProgress = (type, id, ingredient, checked) => {
     }
     window.localStorage.setItem('inProgressRecipes', JSON.stringify(newObj));
     return newObj[key][id].length;
-    // }
   }
   const newObj = { [key]: { [id]: [ingredient] } };
   window.localStorage.setItem('inProgressRecipes', JSON.stringify(newObj));
