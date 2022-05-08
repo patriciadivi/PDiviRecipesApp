5d83a09bc7bd784a6672513fc1a849c1cb9d60ff
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-08 08:29:16 -0300

    Req-54: Create function getDoneREcipes.

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import getDoneRecipes from '../services/getDoneRecipes';
 import '../styles/components/Header.css';
 
 export default function DoneRecipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => { getDoneRecipes(); setRecipes(() => []); }, []);
+  console.log(recipes);
   return (
     <div>
       <Header title="Done Recipes" searchEnabled={ false } />
