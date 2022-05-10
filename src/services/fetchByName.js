bd7d1bacdbe6ad5597b003ab4dcda60b35bd863f
Juliana Giusti <juliana.vmelo@gmail.com>
2022-05-10 16:50:24 -0300

    17

--- src/services/fetchByName.js
+++ src/services/fetchByName.js
@@ -1,4 +1,5 @@
 const fetchByName = async (type, value) => {
+  const numberOfRecepies = 12;
   let url = '';
   if (type === 'foods') {
     url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
@@ -11,7 +12,7 @@ const fetchByName = async (type, value) => {
   if (type === 'foods') {
     return dataByName.meals;
   } if (type === 'drinks') {
-    return dataByName.drinks;
+    return dataByName.drinks.slice(0, numberOfRecepies);
   }
 };
 
