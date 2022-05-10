34db77f600b19c93663ebcac38466f35fb597b4d
PatriciaDivi <patriciavianad@gmail.com>
2022-05-10 19:42:04 -0300

    64, 65, 66 Requisitos finalizados | :rocket:

--- src/pages/FavoriteRecipies.jsx
+++ src/pages/FavoriteRecipies.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
-// import Button from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { useHistory, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionsFavoriteConvertLocalStorage } from '../Redux/actions/index';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
-
 import Header from '../components/Header';
-import ButtonFavoriteCategory from '../components/ButtonFavoriteCategory';
-
+// import ButtonFavoriteCategory from '../components/ButtonFavoriteCategory';
 import '../styles/components/Header.css';
 
 export default function FavoriteRecipies() {
+  const favoriteLocalStorange = useSelector((state) => state
+    .reducer1.favoriteLocalStorange);
   const [favoriteSave, setFavoriteSave] = useState([]);
   const [copyTextToShow, setCopyTextToShow] = useState(false);
   const numberRemoveTextToScreen = 3000;
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const removeTextToScreen = () => {
     setCopyTextToShow(() => false);
@@ -49,34 +54,99 @@ export default function FavoriteRecipies() {
     return newFavorite;
   };
 
+  const dispatchConvertLocalStorage = (favoriteConvertLocalStorage) => {
+    console.log('dispatchConvertLocalStorage');
+    dispatch(actionsFavoriteConvertLocalStorage(favoriteConvertLocalStorage));
+  };
+
   useEffect(() => {
     const favorite = localStorage.getItem('favoriteRecipes');
     if (favorite) {
-      console.log(favorite);
-      setFavoriteSave(JSON.parse(favorite));
+      const favoriteConvertLocalStorage = JSON.parse(favorite);
+      setFavoriteSave(favoriteConvertLocalStorage);
+      dispatchConvertLocalStorage(favoriteConvertLocalStorage);
     }
-  },
-  []);
+  }, []);
+
+  const handleClick = (paramentButton) => {
+    const favoriteSaveFilter = favoriteSave;
+    const filterCategory = favoriteSaveFilter
+      .filter((e) => e.type === paramentButton);
+
+    const filterCategoryAll = favoriteLocalStorange.filter((e) => e);
+
+    if (paramentButton === 'food' || paramentButton === 'drink') {
+      console.log(paramentButton);
+      console.log('filterCategory', filterCategory);
+      return setFavoriteSave(filterCategory);
+    } if (paramentButton === 'all') {
+      console.log('filterCategoryAll', filterCategoryAll);
+      return setFavoriteSave(filterCategoryAll);
+    }
+  };
+
+  const toDateions = (id, type) => {
+    // const { id } = e.target;
+    // console.log(target);
+    console.log(type);
+    console.log(id);
+    if (type === 'drink') {
+      return history.push(`/drinks/${id}`);
+    }
+    return history.push(`/foods/${id}`);
+  };
 
   return (
-    <div>
+    <section>
       <Header title="Favorite Recipes" searchEnabled={ false } />
       <h2>Suas Comidas Favoritas</h2>
-      <ButtonFavoriteCategory />
+      {/* <ButtonFavoriteCategory /> */}
+      <div>
+
+        <Button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => handleClick('all') }
+        >
+          All
+        </Button>
+
+        <Button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => handleClick('food') }
+        >
+          Foods
+        </Button>
+
+        <Button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => handleClick('drink') }
+        >
+          Drinks
+        </Button>
+      </div>
 
       {favoriteSave.map((like, index) => (
         <div
           key={ like.id }
         >
-          <img
-            id={ like.id }
-            src={ like.image }
-            alt={ `Receita de
-              ${like.name}` }
-            width="100"
-            height="100"
-            data-testid={ `${index}-horizontal-image` }
-          />
+          <button
+            type="button"
+            onClick={ () => toDateions(like.id, like.type) }
+          >
+            <img
+              id={ like.id }
+              type={ like.type }
+              src={ like.image }
+              alt={ `Receita de
+            ${like.name}` }
+              width="100"
+              height="100"
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </button>
           { like.type === 'drink' && (
             <p
               data-testid={ `${index}-horizontal-top-text` }
@@ -85,11 +155,17 @@ export default function FavoriteRecipies() {
               {like.alcoholicOrNot}
             </p>
           )}
-          <h4
-            data-testid={ `${index}-horizontal-name` }
-          >
-            {like.name}
-          </h4>
+          <Link to={ `/${like.type}s/${like.id}` }>
+            {/* type="button"
+            onClick={ () => toDateions(like.id, like.type) } */}
+            <h4
+              data-testid={ `${index}-horizontal-name` }
+              id={ like.id }
+              type={ like.type }
+            >
+              {like.name}
+            </h4>
+          </Link>
 
           {like.type === 'food' && (
             <p
@@ -126,6 +202,6 @@ export default function FavoriteRecipies() {
           </button>
         </div>
       ))}
-    </div>
+    </section>
   );
 }
